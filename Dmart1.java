c2686a322edaf22b94730dd41ec0de68f0024872
Navya1197 <xworkz.navya@gmail.com>
2022-11-27 16:13:55 +0530

    committing arraywithforloop related files

---
+++ Dmart1.java
@@ -0,0 +1,20 @@
+class DMart1
+{
+static String vegetables[] = {"carrot", "cabbage", "onion", "radish" , "cucumber" , "pumpkin", "bitterguard", "beans" , "potato", "beetroot"};
+static String biscuits[] ={"Britannia", "parle g", "sunfeast", "Marie", "Horlicks", " hide and seek", " bourbon", "buttercookies", " oreo", "patanjali"};
+public static void main (String navya[])
+{
+System.out.println(vegetables.length);
+System.out.println(biscuits.length);
+
+for(int i=0; i<vegetables.length; i++)
+{
+System.out.println(vegetables[i]);
+}
+for (int j=0 ; j< biscuits.length; j++)
+{
+System.out.println(biscuits[j]);
+
+}
+}
+}
