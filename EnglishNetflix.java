1dbfdcde8bfd524f68e056a58e637fdb2ce7c408
Navya1197 <xworkz.navya@gmail.com>
2022-11-27 16:20:37 +0530

    committing arraywithforloop related files

---
+++ EnglishNetflix.java
@@ -0,0 +1,14 @@
+class EnglishNetflix
+{
+static String netflix[]={"ozark", "the women in house", "sweet mangonolias" ,"inventing anna", "brigade", "bridgerton", "the ultimatatum", "heartstopper", "anatomy","russian" };
+
+public static void main (String args[])
+{
+for( int i=0; i<netflix.length; i++)
+{
+System.out.println(netflix[i]);	
+	
+	
+}
+}
+}
